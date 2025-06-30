586533b30d89182fecb42906854b12ab2e05be44
Shahnawaz-AbdulRehman <shahnawazabdulrehman@gmail.com>
2025-06-30 20:38:03 +0500

    YouTube video added

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 
-const videos = ["/video1.mp4", "/video2.mp4", "/video3.mp4"];
-
 export default function Hero() {
-  const [videoSrc, setVideoSrc] = useState("");
-
-  useEffect(() => {
-    const random = Math.floor(Math.random() * videos.length);
-    setVideoSrc(videos[random]);
-  }, []);
-
   return (
     <motion.section
       id="hero"
@@ -21,18 +11,23 @@ export default function Hero() {
       transition={{ duration: 1 }}
       className="relative min-h-[500px] lg:min-h-screen flex items-center justify-center text-center overflow-hidden"
     >
-      {/* Background Video */}
-      {videoSrc && (
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="absolute inset-0 w-full h-full object-cover"
-        >
-          <source src={videoSrc} type="video/mp4" />
-        </video>
-      )}
+      {/* YouTube Background Video */}
+      <div className="absolute inset-0 w-full h-full z-0 overflow-hidden">
+        <div className="absolute top-0 left-0 w-full h-full">
+           <iframe
+          className="absolute top-1/2 left-1/2"
+          style={{
+            width: "1905px",
+            height: "1071.56px",
+            transform: "translate(-50%, -50%)",
+          }}
+          src="https://www.youtube.com/embed/xLq5FH0gYb4?si=-en4RRM9HJRk2rpW&autoplay=1&mute=1&controls=0&loop=1&playlist=xLq5FH0gYb4&modestbranding=1&rel=0&showinfo=0&playsinline=1"
+          allow="autoplay; fullscreen"
+          allowFullScreen
+          title="Integration Background Video"
+        ></iframe>
+        </div>
+      </div>
 
       {/* Overlay Content */}
       <div className="text-white relative z-10 px-6 max-w-xl lg:max-w-2xl text-white">
@@ -48,7 +43,7 @@ export default function Hero() {
       </div>
 
       {/* Optional dark overlay */}
-      <div className="absolute inset-0 bg-black/50" />
+      <div className="absolute inset-0 bg-black/50 z-[1]" />
     </motion.section>
   );
 }
